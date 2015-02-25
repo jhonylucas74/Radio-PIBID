143a0d4a183d65475d45fb40a2118191f11b177c
Jhony Lucas <jhonyjl37@gmail.com>
2015-02-25 05:53:27 -0300

    primeiro commit

---
+++ www/js/app.js
@@ -0,0 +1,105 @@
+var app = angular.module('starter', ['ionic','ngFx']);
+
+app.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+
+app.controller("Stream",function($scope,$timeout){
+  $scope.play = true;
+  $scope.loading = false;
+  $scope.stop = false;
+
+
+  document.getElementById("loading").style.display = 'none';
+  document.getElementById("stop").style.display = 'none';
+
+  $scope.change_status = function(){
+
+    if( $scope.play == true){
+      $scope.play = false;
+      $scope.loading = true;
+
+      document.getElementById("play").style.display = 'none';
+      document.getElementById("loading").style.display = 'block';
+      document.getElementById("message").innerHTML = "Carregando stream..";
+      playAudio();
+    }
+
+    if( $scope.stop == true){
+      $scope.stop = false;
+      $scope.play = true;
+      document.getElementById("stop").style.display = 'none';
+      document.getElementById("play").style.display = 'block';
+      document.getElementById("message").innerHTML = "Toque no botão play <br> para ouvir a rádio.";
+      stopAudio();
+    }
+  };
+
+  function endLoading(){
+    $scope.loading = false;
+    $scope.stop = true;
+    document.getElementById("loading").style.display = 'none';
+    document.getElementById("stop").style.display = 'block';
+  }
+
+  var my_media = null;
+  var mediaTimer = null;
+
+  
+
+  function playAudio(){
+    my_media = new Media("http://50.22.218.101:11232/;", onSuccess, onError);
+    my_media.play();
+
+    if(mediaTimer == null){
+      // Update media position every second
+      mediaTimer = setInterval(function () {
+        // get media position
+        my_media.getCurrentPosition(
+            // success callback
+            function (position) {
+                if (position > -1) {
+                    if($scope.loading == true ){
+                      endLoading();
+                    }
+                    document.getElementById("message").innerHTML = (position) + " sec";
+                }
+            },
+            // error callback
+            function (e) {
+                console.log("Error getting pos=" + e);
+            }
+        );
+      }, 1000);
+    }
+
+  }
+
+  function stopAudio() {
+    if (my_media) {
+        my_media.stop();
+    }
+    clearInterval(mediaTimer);
+    mediaTimer = null;
+  }
+
+
+  function onSuccess(){ }
+
+  function onError(){
+    document.getElementById("message").innerHTML = "Desculpe ocorreu um erro!<br> Tente novamente ^-^"
+  }
+
+  
+
+
+});
